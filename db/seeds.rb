75ea1daaa450d5724b0b0f12aaf4b05e2d8ceed6
Connor Cowan <ccowan003@comcast.net>
2021-10-04 15:54:31 -0700

    added initial seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-bobby = Customer.create(username: password:)
+bobby = Customer.create(username: "bobbyboy", password: "safepassword")
+josie = Customer.create(username: "josiequeen", password: "safestpassword")
 
 
-penguin1 = Penguin.create(name: origin: price: category: bio: image_url:)
+penguin1 = Penguin.create(name: "African Penguin" origin: "South Africa" price: 2000
+                        category: "animal" bio: "A VERY large boy" 
+                        image_url: "")
+
+penguin2 = Penguin.create(name: "King Penguin" origin: "Antarctica" price: 7000
+                        category: "animal" bio: "Has the largest breeding cycle of any bird (16 months)" 
+                        image_url: "")
+
+penguin3 = Penguin.create(name: "Macaroni Penguin" origin: "Antarctic Peninsula" price: 3000
+                        category: "animal" bio: "" 
+                        image_url: "")
+
+penguin4 = Penguin.create(name: "Emperor Penguin" origin: "Antarctica" price: 15000
+                        category: "animal" bio: "" 
+                        image_url: "")
+
+penguin5 = Penguin.create(name: "Adélie penguin" origin: "Antarctica" price: 9000
+                        category: "animal" bio: "" 
+                        image_url: "")
+
+penguin6 = Penguin.create(name: "Galapagos Penguin" origin: "Galapagos Islands" price: 32000
+                        category: "animal" bio: "" 
+                        image_url: "")
+
+
+supply1 =
+
+supply2 =
+
+supply3 = 
+
+supply4 = 
